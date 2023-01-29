a654f5415d3bb5d25c499759e14a60253fba8abc
ziluxike <leimujiang233@gmail.com>
2023-01-30 07:10:47 +0800

    init..

---
+++ src/main/java/com/ultikits/plugins/gm/listeners/GameModeListeners.java
@@ -0,0 +1,10 @@
+package com.ultikits.plugins.gm.listeners;
+
+import com.ultikits.ultitools.listeners.InventoryListener;
+
+/**
+ * Author: ziluxike
+ * Time: 2023/1/30 06:55
+ */
+public class GameModeListeners extends InventoryListener {
+}
