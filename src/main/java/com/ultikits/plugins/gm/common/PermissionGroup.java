46c519b3aa59cd36f7c0d3e5f51585a0cb6e7acc
ziluxike <leimujiang233@gmail.com>
2023-01-30 08:37:52 +0800

    update..

--- src/main/java/com/ultikits/plugins/gm/common/PermissionGroup.java
+++ src/main/java/com/ultikits/plugins/gm/common/PermissionGroup.java
@@ -3,6 +3,9 @@ package com.ultikits.plugins.gm.common;
 /**
  * Author: ziluxike
  * Time: 2023/1/30 07:30
+ * <p>
+ *     使用常量代替，不容易书写错误
+ * </p>
  */
 public class PermissionGroup {
     public final static String SURVIVAL = "ultikits.tools.gm.survival";
